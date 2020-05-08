9e340d620c07ebd0525483f435120cda73281c12
altangerel <mr.tuulai@gmail.com>
2020-05-08 07:34:50 +0200

    solved compareTheTriplets

---
+++ src/hackerrank/CompareTheTriplets.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let aSum=0, bSum=0, i, j, result;
+
+    for(i=0; i<a.length; i++){
+        if(a[i]>b[i]){
+            aSum++;
+        }
+        if(a[i]<b[i]){
+            bSum++;
+        }
+    }
+    result = [aSum, bSum]
+    return result
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
